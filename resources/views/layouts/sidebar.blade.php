2cc08b2b3f2d68222b60284717973f79bde2db6b
Liyundzira <liyundzira.h@gmail.com>
2025-09-08 11:16:13 +0700

    filtering fitur sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,6 +6,7 @@
 					<!-- Sidebar menu starts -->
 					<div class="sidebarMenuScroll">
 						<ul class="sidebar-menu">
+							<li class="menu-header">Halaman</li>
 							<li class="active current-page">
 								<a href="index.html">
 									<i class="bi bi-pie-chart"></i>
@@ -15,55 +16,31 @@
 							<li>
 								<a href="analytics.html">
 									<i class="bi bi-bar-chart-line"></i>
-									<span class="menu-text">Analytics</span>
+									<span class="menu-text">Pengguna</span>
 								</a>
 							</li>
 							<li>
 								<a href="tables.html">
 									<i class="bi bi-border-all"></i>
-									<span class="menu-text">Tables</span>
+									<span class="menu-text">Divisi</span>
 								</a>
 							</li>
 							<li>
 								<a href="events.html">
 									<i class="bi bi-calendar4"></i>
-									<span class="menu-text">Events</span>
+									<span class="menu-text">Konfirmasi Pengajuan</span>
 								</a>
 							</li>
 							<li>
 								<a href="profile.html">
 									<i class="bi bi-person-square"></i>
-									<span class="menu-text">Profile</span>
+									<span class="menu-text">Dokumen</span>
 								</a>
 							</li>
 							<li>
 								<a href="maps.html">
 									<i class="bi bi-pin-map"></i>
-									<span class="menu-text">Maps</span>
-								</a>
-							</li>
-							<li>
-								<a href="tabs.html">
-									<i class="bi bi-slash-square"></i>
-									<span class="menu-text">Tabs</span>
-								</a>
-							</li>
-							<li>
-								<a href="modals.html">
-									<i class="bi bi-terminal"></i>
-									<span class="menu-text">Modals</span>
-								</a>
-							</li>
-							<li>
-								<a href="icons.html">
-									<i class="bi bi-textarea"></i>
-									<span class="menu-text">Icons</span>
-								</a>
-							</li>
-							<li>
-								<a href="typography.html">
-									<i class="bi bi-explicit"></i>
-									<span class="menu-text">Typography</span>
+									<span class="menu-text">Laporan</span>
 								</a>
 							</li>
 					</div>
